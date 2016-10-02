4395a3659aeb454df16811cd4def78d5535ff6a8
Kris <littledream@gmx.com>
2016-10-02 22:57:13 +0800

    Update index.js
    fixed App pathing issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { AppContainer } from 'react-hot-loader';
 
-import App from './components/app';
+import App from './components/App';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
