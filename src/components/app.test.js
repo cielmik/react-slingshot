c143bb83b4446b8c7968b3c973befe95cade968f
Kris <littledream@gmx.com>
2016-10-02 22:18:52 +0800

    Update app.test.js
    Fixed App path on test.

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import test from 'ava';
 import {shallow} from 'enzyme';
-import App from './app';
+import App from './App';
 
-test('it works please', t => {
+test('it passes', t => {
   const wrapper = shallow(<App/>);
   t.is(wrapper.find('div').text(), 'React simple starter');
 });
